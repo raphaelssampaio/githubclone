417e9c59ca99764042bd8a3f7e246a0693bc4a51
Raphael <raphaelssampaio@gmail.com>
2019-12-07 10:50:37 -0300

    Deploying app

---
+++ public/precache-manifest.e5e7fb5cf54a4cad4fb29a7006548456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f0d630cadce90c86f25524f0aa341c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b20d6639d9d95596f385",
+    "url": "/static/js/2.c397428f.chunk.js"
+  },
+  {
+    "revision": "3e01856dd46799eedc35ec9da08dd851",
+    "url": "/static/js/2.c397428f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0b811fb011f7833a73e",
+    "url": "/static/js/main.79e6da50.chunk.js"
+  },
+  {
+    "revision": "7518fee7f87dce385b7f",
+    "url": "/static/js/runtime-main.453157e1.js"
+  }
+]);
